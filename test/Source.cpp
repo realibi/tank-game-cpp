eb28e72e299725eb54322cb9a2c391ad68e72068
realibi <reallibi@gmail.com>
2020-05-17 00:16:00 +0600

    tank models created

--- test/Source.cpp
+++ test/Source.cpp
@@ -3,23 +3,96 @@
 #include <windows.h>
 using namespace std;
 
-#define ROWS 5
-#define COLUMNS 5
+#define ZERO 0
+#define ROWS 10
+#define COLUMNS 20
+
+enum CellStates {
+	ACTIVE = 1,
+	NONACTIVE = 0
+};
 
 enum Buttons {
-	left = 97,
-	up = 119,
-	right = 100,
-	down = 115
+	LEFT = 97,
+	UP = 119,
+	RIGHT = 100,
+	DOWN = 115,
+	ENTER = 13
+};
+
+enum Rotation {
+	TO_LEFT,
+	TO_UP,
+	TO_RIGHT,
+	TO_DOWN
 };
 
 struct Position {
 	int posX;
 	int posY;
+
+	Position(){}
+
+	Position(int posX, int poosY) {
+		this->posX = posX;
+		this->posY = posY;
+	}
 };
 
-void draw(int map[ROWS][COLUMNS]) {
+struct Tank {
+	Rotation rotationState;
+	Position gunpointPosition = Position();
+	int health;
+};
+
+void draw(int map[ROWS][COLUMNS], Tank tank1, Tank tank2, int sleepDuration = 0) {
 	system("cls");
+
+	map[tank1.gunpointPosition.posX][tank1.gunpointPosition.posY] = 1;
+	map[tank2.gunpointPosition.posX][tank2.gunpointPosition.posY] = 1;
+
+	if (tank1.rotationState == Rotation::TO_UP) {
+		map[tank1.gunpointPosition.posY + 1][tank1.gunpointPosition.posX - 1] = 1;
+		map[tank1.gunpointPosition.posY + 1][tank1.gunpointPosition.posX] = 1;
+		map[tank1.gunpointPosition.posY + 1][tank1.gunpointPosition.posX + 1] = 1;
+	}
+	else if (tank1.rotationState == Rotation::TO_LEFT) {
+		map[tank1.gunpointPosition.posY - 1][tank1.gunpointPosition.posX + 1] = 1;
+		map[tank1.gunpointPosition.posY][tank1.gunpointPosition.posX + 1] = 1;
+		map[tank1.gunpointPosition.posY + 1][tank1.gunpointPosition.posX + 1] = 1;
+	}
+	else if (tank1.rotationState == Rotation::TO_RIGHT) {
+		map[tank1.gunpointPosition.posY - 1][tank1.gunpointPosition.posX - 1] = 1;
+		map[tank1.gunpointPosition.posY][tank1.gunpointPosition.posX - 1] = 1;
+		map[tank1.gunpointPosition.posY + 1][tank1.gunpointPosition.posX - 1] = 1;
+	}
+	else if (tank1.rotationState == Rotation::TO_DOWN) {
+		map[tank1.gunpointPosition.posY - 1][tank1.gunpointPosition.posX - 1] = 1;
+		map[tank1.gunpointPosition.posY - 1][tank1.gunpointPosition.posX] = 1;
+		map[tank1.gunpointPosition.posY - 1][tank1.gunpointPosition.posX + 1] = 1;
+	}
+
+	if (tank2.rotationState == Rotation::TO_UP) {
+		map[tank2.gunpointPosition.posY + 1][tank2.gunpointPosition.posX - 1] = 1;
+		map[tank2.gunpointPosition.posY + 1][tank2.gunpointPosition.posX] = 1;
+		map[tank2.gunpointPosition.posY + 1][tank2.gunpointPosition.posX + 1] = 1;
+	}
+	else if (tank2.rotationState == Rotation::TO_LEFT) {
+		map[tank2.gunpointPosition.posY - 1][tank2.gunpointPosition.posX + 1] = 1;
+		map[tank2.gunpointPosition.posY][tank2.gunpointPosition.posX + 1] = 1;
+		map[tank2.gunpointPosition.posY + 1][tank2.gunpointPosition.posX + 1] = 1;
+	}
+	else if (tank2.rotationState == Rotation::TO_RIGHT) {
+		map[tank2.gunpointPosition.posY - 1][tank2.gunpointPosition.posX - 1] = 1;
+		map[tank2.gunpointPosition.posY][tank2.gunpointPosition.posX - 1] = 1;
+		map[tank2.gunpointPosition.posY + 1][tank2.gunpointPosition.posX - 1] = 1;
+	}
+	else if (tank2.rotationState == Rotation::TO_DOWN) {
+		map[tank2.gunpointPosition.posY - 1][tank2.gunpointPosition.posX - 1] = 1;
+		map[tank2.gunpointPosition.posY - 1][tank2.gunpointPosition.posX] = 1;
+		map[tank2.gunpointPosition.posY - 1][tank2.gunpointPosition.posX + 1] = 1;
+	}
+	
 	for (int i = 0; i < ROWS; i++) {
 		for (int k = 0; k < COLUMNS; k++) {
 			if (map[i][k] == 1) {
@@ -28,6 +101,7 @@ void draw(int map[ROWS][COLUMNS]) {
 			else {
 				cout << (char)176;
 			}
+			Sleep(sleepDuration);
 		}
 		cout << endl;
 	}
@@ -53,41 +127,63 @@ Position getActiveCellPosition(int map[ROWS][COLUMNS]) {
 	return activePosition;
 }
 
-void changeActiveCell(int map[ROWS][COLUMNS], int buttonCode) {
+void player1Move(int map[ROWS][COLUMNS], Tank tank, int buttonCode) {
 	Position activeCellPosition = getActiveCellPosition(map);
 
 	switch (buttonCode) {
-	case Buttons::right:
+	case Buttons::RIGHT:
 		if ((activeCellPosition.posX + 1) != COLUMNS) {
-			map[activeCellPosition.posY][activeCellPosition.posX] = 0;
-			map[activeCellPosition.posY][activeCellPosition.posX + 1] = 1;
+			map[activeCellPosition.posY][activeCellPosition.posX] = CellStates::NONACTIVE;
+			map[activeCellPosition.posY][activeCellPosition.posX + 1] = CellStates::ACTIVE;
 		}
 		break;
-	case Buttons::left:
-		if ((activeCellPosition.posX - 1) >= 0) {
-			map[activeCellPosition.posY][activeCellPosition.posX] = 0;
-			map[activeCellPosition.posY][activeCellPosition.posX - 1] = 1;
+	case Buttons::LEFT:
+		if ((activeCellPosition.posX - 1) >= ZERO) {
+			map[activeCellPosition.posY][activeCellPosition.posX] = CellStates::NONACTIVE;
+			map[activeCellPosition.posY][activeCellPosition.posX - 1] = CellStates::ACTIVE;
 		}
 		break;
-	case Buttons::down:
+	case Buttons::DOWN:
 		if ((activeCellPosition.posY + 1) != ROWS) {
-			map[activeCellPosition.posY][activeCellPosition.posX] = 0;
-			map[activeCellPosition.posY + 1][activeCellPosition.posX] = 1;
+			map[activeCellPosition.posY][activeCellPosition.posX] = CellStates::NONACTIVE;
+			map[activeCellPosition.posY + 1][activeCellPosition.posX] = CellStates::ACTIVE;
 		}
 		break;
-	case Buttons::up:
-		if ((activeCellPosition.posY - 1) >= 0) {
-			map[activeCellPosition.posY][activeCellPosition.posX] = 0;
-			map[activeCellPosition.posY - 1][activeCellPosition.posX] = 1;
+	case Buttons::UP:
+		if ((activeCellPosition.posY - 1) >= ZERO) {
+			map[activeCellPosition.posY][activeCellPosition.posX] = CellStates::NONACTIVE;
+			map[activeCellPosition.posY - 1][activeCellPosition.posX] = CellStates::ACTIVE;
 		}
 		break;
 	}
 }
 
 int main() {
-	int map[ROWS][COLUMNS] = { {1, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0} };
+	int map[ROWS][COLUMNS] = { 
+								{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+								{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+								{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+								{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+								{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+								{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+								{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+								{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+								{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+								{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+							};
+
+	Tank tank1, tank2;
+	tank1.gunpointPosition.posX = 3;
+	tank1.gunpointPosition.posY = 3;
+	tank1.health = 100;
+	tank1.rotationState = Rotation::TO_UP;
+
+	tank2.gunpointPosition.posX = 7;
+	tank2.gunpointPosition.posY = 7;
+	tank2.health = 100;
+	tank2.rotationState = Rotation::TO_LEFT;
 	
-	draw(map);
+	draw(map, tank1, tank2); //draw(map, 10) for starting animation
 
 	int button;
 	while (true)
@@ -96,8 +192,9 @@ int main() {
 		{
 			button = _getch();
 			
-			changeActiveCell(map, button);
-			draw(map);
+			//changeActiveCell(map, button);
+			draw(map, tank1, tank2);
+			cout << "\npressed button: " << (char)button << " (" << button << ")";
 		}
 		Sleep(250);
 	}
